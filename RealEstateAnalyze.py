4160ac2ea97bab4a2e463497bb974ffd1ab2aa6a
dylan1218 <dylan1218@live.com>
2019-11-12 10:08:47 -0500

    Update RealEstateAnalyze.py

--- RealEstateAnalyze.py
+++ RealEstateAnalyze.py
@@ -10,22 +10,24 @@ import numpy as np
 #redfin.Get_Zillow_Data() #get market pricing data
 #redfin.Get_ZillowRent_Data() #gets market rental data
 
+#The DataframeLoad object is utilized to return dataframes based upon the data imported from the RedfinDataRetrieve object
 pandasquery = DataframeLoad()
 
-#ZillowDF = pandasquery.GetZillowDF()
+ZillowDF = pandasquery.GetZillowDF()
 ZillowRentDF = pandasquery.GetZillowRentDF()
 RedfinDF = pandasquery.GetRedfinDF()
+
+
 
 #inputs
 mortgageRate = .045
 propertyTaxRate = .0125
 
+#Adds an average price field -- to do a weighted ranking consider price and rent differential
+avgPriceMergeDF = pd.merge(RedfinDF, ZillowDF, left_on='ZIP OR POSTAL CODE', right_on='RegionName')
+print(avgPriceMergeDF)
 
-#Adds an average price column -- to do a weighted ranking consider price and rent differential
-#avgPriceMergeDF = pd.merge(RedfinDF, ZillowDF, left_on='ZIP OR POSTAL CODE', right_on='RegionName')
-#print(avgPriceMergeDF)
-#Adds an average rent column and other calculated columns
-
+#Adds an average rent column and other calculated fields
 avgRentMergeDF = pd.merge(RedfinDF, ZillowRentDF, left_on='ZipBedKey', right_on='ZipBedKey', how='left')
 print(avgRentMergeDF)
 avgRentMergeDF['PRICE'] = avgRentMergeDF['PRICE'].astype(float)
@@ -40,9 +42,11 @@ depreciationCost = (avgRentMergeDF['PRICE']*.01)/12
 
 monthlyCostEstimate = mortgageEstimate + propertyTaxEstimate + insruanceEstimate + HOACost + depreciationCost
 
+#Adds a monthly cost estimate field
 avgRentMergeDF['monthlyCostEstimate'] = monthlyCostEstimate
 
 avgRentMergeDF['AvgRentVariance'] = avgRentMergeDF['2019-09'] - monthlyCostEstimate
+
 #Print the DF to a csv
 avgRentMergeDF.to_csv("C:/Users/YOURNAME/PandasDFProjects/RedfinData/CSVDownload/Redfin_ZillowMerge.csv")
 
